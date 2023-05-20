b45faa6cf62843c20817d29491399c3534f10982
Somayyah Maqsudi <smaqsudi3@gatech.edu>
2023-05-20 16:49:30 -0400

    1

--- utils/seed.js
+++ utils/seed.js
@@ -1,48 +1,3 @@
-// const connection = require('../config/connection');
-// const { User, Thought } = require('../models');
-
-// connection.on('error', (err) => err);
-
-// connection.once('open', async () => {
-//   console.log('connected');
-
-//   await Thought.deleteMany({});
-
-//   await User.deleteMany({});
-
-//   const users = [
-//       {
-//           username: "fred",
-//           email: "fred@hotmail.com"
-//       },
-//       {
-//           username: "tony",
-//           email: "tony@gmail.com"
-//       }
-//   ];
-
-//   const thoughts = [
-//       {
-//           thoughtText: "Many, many moons ago...",
-//           username: "Steve"
-//       },
-//       {
-//           thoughtText: "How many times must I ...",
-//           username: "Linda"
-//       }
-//   ]
-
-//   await User.collection.insertMany(users);
-
-//   await Thought.collection.insertMany(thoughts);
-
-//   console.table(users);
-//   console.table(thoughts);
-//   console.info('Seeding complete! 🌱');
-//   process.exit(0);
-// });
-
-
 import connection from '../config/connection';
 import { User, Thought } from '../models';
 
@@ -57,23 +12,23 @@ connection.once('open', async () => {
 
     const users = [
       {
-        username: 'fred',
-        email: 'fred@hotmail.com'
+        username: 'frjohned',
+        email: 'john@hotmail.com'
       },
       {
-        username: 'tony',
-        email: 'tony@gmail.com'
+        username: 'sam',
+        email: 'sam@gmail.com'
       }
     ];
 
     const thoughts = [
       {
         thoughtText: 'Many, many moons ago...',
-        username: 'Steve'
+        username: 'Kim'
       },
       {
         thoughtText: 'How many times must I...',
-        username: 'Linda'
+        username: 'Chris'
       }
     ];
 
